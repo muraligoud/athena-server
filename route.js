88371f0fc60377d42d0ff2e415966cfef696d025
Murali <murali@elpaviation.com>
2023-08-03 04:54:28 -0500

    added scripts in package.json file

--- route.js
+++ route.js
@@ -3,6 +3,10 @@ const userDetailsRoute = require('./routes/user-details/user_details.route')
 
 const router = express.Router();
 
+router.use('/', (req, res) => {
+    res.send('hello, athena server')
+})
+
 
 router.use('/user-details', userDetailsRoute)
 
